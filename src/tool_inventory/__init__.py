333e2f839ff2e5e7b4a97045ee7da804fafd4e99
Micael Jarniac <micael@jarniac.com>
2024-11-26 11:38:08 -0300

    feat: basic functionality
    Closes #3
    Closes #6
    Related to #8

--- src/tool_inventory/__init__.py
+++ src/tool_inventory/__init__.py
@@ -1,5 +1,1 @@
 """Tool inventory."""
-
-__all__ = ["make_greeting"]
-
-from ._tool_inventory import make_greeting
---
+++ tests/test_dummy.py
@@ -0,0 +1,7 @@
+"""Dummy test."""
+
+from __future__ import annotations
+
+
+def test_dummy() -> None:
+    """Dummy test."""
